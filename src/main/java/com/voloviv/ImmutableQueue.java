90d2d0a353e89e32ef74c08303eb3024cd1cc504
Vitaly <vvolovich@panvistamobile.com>
2016-11-22 15:42:31 -0500

    fixed spacing

--- src/main/java/com/voloviv/ImmutableQueue.java
+++ src/main/java/com/voloviv/ImmutableQueue.java
@@ -10,176 +10,176 @@ import java.util.NoSuchElementException;
  */
 public class ImmutableQueue<T> implements Queue<T>
 {
-	private static class Stack<T>{
-		
-		private T head;
-		private Stack<T> tail;
-		private int size;
-		
-		/**
-		 * Default constructor
-		 * 
-		 * */
-		private Stack(){
-			this.head = null;
-			this.tail = null;
-			this.size = 0;
-		}
-		
-		/**
-		 * Constructor Overloading 
-		 * 
-		 * @param T obj
-		 * @param Stack tail
-		 * 
-		 * */
-		private Stack(T obj,Stack<T> tail){
-			this.head = obj;
-			this.tail = tail;
-			this.size = tail.size+1;
-		}
-		
-		
-		/**
-		 * Check if stack is empty
-		 * 
-		 * @return true or false depending on whether stack is empty
-		 * 
-		 * */
-		public boolean isEmpty(){
-			return this.size == 0;
-		}
-		
-		/**
-		 * Push new element into stack
-		 * 
-		 * @param T object
-		 * @return Stack
-		 * 
-		 * */
-		public Stack<T> push(T obj){
-			return new Stack<T>(obj,this);
-		}
-		
-		/**
-		 * Reverse the order of the stack
-		 * 
-		 * @return Stack reversed stack
-		 * 
-		 * */
-		public Stack<T> reverseStack(){
-			Stack<T> stack = new Stack<T>();
-			Stack<T> tail = this;
-			while(!tail.isEmpty()){
-				stack = stack.push(tail.head);
-				tail = tail.tail;
-			}
-			return stack;
-		}
-	}
-	
-	private Stack<T> forward;
-	private Stack<T> reverse;
-	
-	/**
-	 * Constructor
-	 * 
-	 * Create 2 Stack objects for opposite ordering of queue elements
-	 * 
-	 * */
-	public ImmutableQueue(){
-		this.forward = new Stack<T>();
-		this.reverse = new Stack<T>();
-	}
-	
-	/**
-	 * Constructor overloading 
-	 * 
-	 * @param Stack forward
-	 * @param Stack reverse
-	 * 
-	 * */
-	public ImmutableQueue(Stack<T> forward,Stack<T> reverse){
-		this.forward = forward;
-		this.reverse = reverse;
-	}
-	
-	/**
-	 * Add object to queue
-	 * Throws IllegalArgumentException
-	 * 
-	 * @return ImmutableQueue
-	 *  
-	 * */
-	public Queue<T> enQueue(T object){
-		if(object!=null) {
-			return new ImmutableQueue<T>(this.forward.push(object),this.reverse);
-		}
-		throw new IllegalArgumentException();
-	}
-	
-	/**
-	 * Reverse the "forward" stack, and assign 
-	 * new stack to the "reverse" instance variable.
-	 * Used by the "head" to obtain the first element
-	 * in the queue
-	 * 
-	 * */
-	private void forwardToReverse(){
-		this.reverse= this.forward.reverseStack();
-		this.forward = new Stack<T>();
-	}
-
-	/**
-	 * Remove the first element in the queue
-	 * 
-	 * @return Queue
-	 * */
-	public Queue<T> deQueue(){
-		if(this.isEmpty())
-			throw new NoSuchElementException();
-		if(!this.reverse.isEmpty()){
-			return new ImmutableQueue<T>(this.forward,this.reverse.tail);
-		}else{
-			return new ImmutableQueue<T>(new Stack<T>(),this.forward.reverseStack().tail);
-		}		
-	}
-	
-	/**
-	 * Return the first element in the queue
-	 * 
-	 * @return T first element
-	 * */
-    public T head(){
-    	if (this.isEmpty())
-			throw new NoSuchElementException();
-		if (this.reverse.isEmpty())
-			forwardToReverse();
-		return this.reverse.head;
-    };
+    private static class Stack<T>{
+        
+        private T head;
+        private Stack<T> tail;
+        private int size;
+        
+        /**
+         * Default constructor
+         * 
+         * */
+        private Stack(){
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+        
+        /**
+         * Constructor Overloading 
+         * 
+         * @param T obj
+         * @param Stack tail
+         * 
+         * */
+        private Stack(T obj,Stack<T> tail){
+            this.head = obj;
+            this.tail = tail;
+            this.size = tail.size+1;
+        }
+        
+        
+        /**
+         * Check if stack is empty
+         * 
+         * @return true or false depending on whether stack is empty
+         * 
+         * */
+        public boolean isEmpty(){
+            return this.size == 0;
+        }
+        
+        /**
+         * Push new element into stack
+         * 
+         * @param T object
+         * @return Stack
+         * 
+         * */
+        public Stack<T> push(T obj){
+            return new Stack<T>(obj,this);
+        }
+        
+        /**
+         * Reverse the order of the stack
+         * 
+         * @return Stack reversed stack
+         * 
+         * */
+        public Stack<T> reverseStack(){
+            Stack<T> stack = new Stack<T>();
+            Stack<T> tail = this;
+            while(!tail.isEmpty()){
+                stack = stack.push(tail.head);
+                tail = tail.tail;
+            }
+            return stack;
+        }
+    }
+    
+    private Stack<T> forward;
+    private Stack<T> reverse;
     
     /**
-	 * Return the last element in the queue
-	 * 
-	 * @return T last element
-	 * */
-    public T tail(){
-    	if (this.isEmpty())
-			throw new NoSuchElementException();
-		if (this.forward.isEmpty()){
-			return this.reverse.reverseStack().head;
-		} else {
-			return this.forward.head;
-		}
+     * Constructor
+     * 
+     * Create 2 Stack objects for opposite ordering of queue elements
+     * 
+     * */
+    public ImmutableQueue(){
+        this.forward = new Stack<T>();
+        this.reverse = new Stack<T>();
     }
     
     /**
-	 * Get queue size (equal to length of forward + reverse stack)
-	 * 
-	 * @return boolean queue size
-	 * */
+     * Constructor overloading 
+     * 
+     * @param Stack forward
+     * @param Stack reverse
+     * 
+     * */
+    public ImmutableQueue(Stack<T> forward,Stack<T> reverse){
+        this.forward = forward;
+        this.reverse = reverse;
+    }
+    
+    /**
+     * Add object to queue
+     * Throws IllegalArgumentException
+     * 
+     * @return ImmutableQueue
+     *  
+     * */
+    public Queue<T> enQueue(T object){
+        if(object!=null) {
+            return new ImmutableQueue<T>(this.forward.push(object),this.reverse);
+        }
+        throw new IllegalArgumentException();
+    }
+    
+    /**
+     * Reverse the "forward" stack, and assign 
+     * new stack to the "reverse" instance variable.
+     * Used by the "head" to obtain the first element
+     * in the queue
+     * 
+     * */
+    private void forwardToReverse(){
+        this.reverse= this.forward.reverseStack();
+        this.forward = new Stack<T>();
+    }
+
+    /**
+     * Remove the first element in the queue
+     * 
+     * @return Queue
+     * */
+    public Queue<T> deQueue(){
+        if(this.isEmpty())
+            throw new NoSuchElementException();
+        if(!this.reverse.isEmpty()){
+            return new ImmutableQueue<T>(this.forward,this.reverse.tail);
+        }else{
+            return new ImmutableQueue<T>(new Stack<T>(),this.forward.reverseStack().tail);
+        }        
+    }
+    
+    /**
+     * Return the first element in the queue
+     * 
+     * @return T first element
+     * */
+    public T head(){
+        if (this.isEmpty())
+            throw new NoSuchElementException();
+        if (this.reverse.isEmpty())
+            forwardToReverse();
+        return this.reverse.head;
+    };
+    
+    /**
+     * Return the last element in the queue
+     * 
+     * @return T last element
+     * */
+    public T tail(){
+        if (this.isEmpty())
+            throw new NoSuchElementException();
+        if (this.forward.isEmpty()){
+            return this.reverse.reverseStack().head;
+        } else {
+            return this.forward.head;
+        }
+    }
+    
+    /**
+     * Get queue size (equal to length of forward + reverse stack)
+     * 
+     * @return boolean queue size
+     * */
     public boolean isEmpty(){
-    	return this.forward.size + this.reverse.size == 0;
+        return this.forward.size + this.reverse.size == 0;
     };
     
 }
--- src/test/java/com/voloviv/ImmutableQueueTest.java
+++ src/test/java/com/voloviv/ImmutableQueueTest.java
@@ -14,53 +14,53 @@ import com.voloviv.ImmutableQueue;
 public class ImmutableQueueTest 
 {
     private final ArrayList<Integer> data = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-	
+    
     @Test
     public void enQueueShoudlReturnNewQueueInstanceWithNewElementAdded(){
-		
-		Queue<Integer> queue = new ImmutableQueue<Integer>();
-    	for (Integer i : data){
-    		Queue<Integer> old_queue = queue;
-    		queue = queue.enQueue(i);
-    		assertTrue(old_queue != queue);
-    		assertTrue(queue.tail() == i);
-    	};
+        
+        Queue<Integer> queue = new ImmutableQueue<Integer>();
+        for (Integer i : data){
+            Queue<Integer> old_queue = queue;
+            queue = queue.enQueue(i);
+            assertTrue(old_queue != queue);
+            assertTrue(queue.tail() == i);
+        };
     }
     
     @Test
     public void deQueueShouldReturnANewQueueInstanceWithTopElementRemoved(){
-    	
-    	Queue<Integer> queue = new ImmutableQueue<Integer>();
-    	for (Integer i : data){
-    		queue = queue.enQueue(i);
-    	};
-    	
-    	Queue<Integer> old_queue = queue;
-    	queue = queue.deQueue();
-    	assertTrue(old_queue != queue);
-    	assertTrue(queue.head() == data.get(1));
-    	
+        
+        Queue<Integer> queue = new ImmutableQueue<Integer>();
+        for (Integer i : data){
+            queue = queue.enQueue(i);
+        };
+        
+        Queue<Integer> old_queue = queue;
+        queue = queue.deQueue();
+        assertTrue(old_queue != queue);
+        assertTrue(queue.head() == data.get(1));
+        
     }
     
     @Test
     public void getHeadShouldReturnTheFirstElementInQueue(){
-		
-		Queue<Integer> queue = new ImmutableQueue<Integer>();
-    	for (Integer i : data){
-    		queue = queue.enQueue(i);
-    	};
-    	
-    	assertTrue(queue.head() == data.get(0));
+        
+        Queue<Integer> queue = new ImmutableQueue<Integer>();
+        for (Integer i : data){
+            queue = queue.enQueue(i);
+        };
+        
+        assertTrue(queue.head() == data.get(0));
     }
     
     @Test
     public void getTailShouldReturnTheLastElementInQueue(){
-		
-		Queue<Integer> queue = new ImmutableQueue<Integer>();
-    	for (Integer i : data){
-    		queue = queue.enQueue(i);
-    	};
-    	
-    	assertTrue(queue.tail() == data.get(data.size()-1));
+        
+        Queue<Integer> queue = new ImmutableQueue<Integer>();
+        for (Integer i : data){
+            queue = queue.enQueue(i);
+        };
+        
+        assertTrue(queue.tail() == data.get(data.size()-1));
     }
 }
